be6aa6b382bb8bf334d9bcb254298f40fe8ea139
Joe Zagorski <j.j.zagorski@gmail.com>
2018-03-21 21:19:30 -0400

    removeing columns to work in PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20180322010450) do
     t.string "security_answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.references :users   
   end
 
