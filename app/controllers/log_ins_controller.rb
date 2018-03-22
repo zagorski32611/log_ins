022fb29e9a4ad7de44245ac5bbc1417dcacc0dff
Joe Zagorski <j.j.zagorski@gmail.com>
2018-03-22 19:19:29 -0400

    refactored controller

--- app/controllers/log_ins_controller.rb
+++ app/controllers/log_ins_controller.rb
@@ -1,6 +1,19 @@
 class LogInsController < ApplicationController
     before_action :authenticate_user!
+    before_action :fetch_log_in, only: [:show, :update, :destroy, :edit]
+    before_action :verify_user, only: [:show, :update, :destroy, :edit]
     
+    def fetch_log_in
+        @log_in = LogIn.find(params[:id])
+    end
+
+    def verify_user
+        unless @log_in.user_id == current_user.id
+            flash[:error] = "I'm sorry, Dave..."
+            redirect_to root_path
+        end
+    end
+
     def index
         @log_in = LogIn.where(user_id: current_user.id)
     end
@@ -13,20 +26,18 @@ class LogInsController < ApplicationController
         @log_in = LogIn.new(log_in_params)
         @log_in.user_id = current_user.id
         if @log_in.save(log_in_params)
-          flash[:notice] = "Successfully created post!"
-          redirect_to log_in_path(@log_in)
+            flash[:notice] = "Successfully created post!"
+            redirect_to log_in_path(@log_in)
         else
-          flash[:alert] = "Error creating new post!"
-          render :new
+            flash[:alert] = "Error creating new post!"
+            render :new
         end
-      end
+    end
     
     def edit
-        @log_in = LogIn.find(params[:id])
     end
 
     def update
-        @log_in = LogIn.find(params[:id])
         if @log_in.update(log_in_params)
             flash[:notice] = "Successfully updated log in!"
             redirect_to root_path
@@ -37,12 +48,9 @@ class LogInsController < ApplicationController
     end
 
     def show
-        @log_ins = LogIn.find(params[:id])
-        #@log_ins = @log_ins.current_user.find([:id])
     end
 
     def destroy
-        @log_ins = LogIn.find(params[:id])
         if @log_ins.destroy
             flash[:notice] = "Successfully delete log in!"
             redirect_to root_path
